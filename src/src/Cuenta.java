e79e6b0c2259df2460c1ade97fddfeaf8fd9f5c3
Toni Revuelta <>
2021-11-12 09:54:49 +0100

    paso 1

---
+++ src/src/Cuenta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author usuario
+ */
+public class Cuenta {
+    private float balance;
+    public void sacarDinero(float dinero){
+        
+    }
+    public void meterDinero(float dinero){
+        
+    }
+}
